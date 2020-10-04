a901cc9a0b6efe8019ee3efcef93ae23213f129b
k-yler <68955748+k-yler@users.noreply.github.com>
2020-10-04 22:23:15 +0300

    Add files via upload

--- house/js/script.js
+++ house/js/script.js
@@ -1,40 +1,27 @@
 $(document).ready(function () {
-  $(".slider").slick({
-    arrows: false,
-    dots: false,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    veriableWigth: true,
-    responsive: [
-      {
-        breakpoint: 1120,
-        settings: {
-          slidesToShow: 3,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 990,
-        settings: {
-          slidesToShow: 2,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 767,
-        settings: {
-          slidesToShow: 1,
-          dots: true,
-        },
-      },
-    ],
+  $("#menu").on("click", "a", function (event) {
+    event.preventDefault();
+    var id = $(this).attr("href"),
+      top = $(id).offset().top;
+    $("body,html").animate({ scrollTop: top }, 1000);
   });
 
- $("#menu").on("click", "a", function (event) {
-          event.preventDefault();
-          var id = $(this).attr("href"),
-            top = $(id).offset().top;
-          $("body,html").animate({ scrollTop: top }, 1000);
-        });
-
+  $(".slidersone").slick({
+    arrows: false,
+    asNavFor: ".slidersthree",
+  });
+});
+// $(document).ready(function () {
+//   $(".sliderstwo").slick({
+//     arrows: false,
+//     fade: true,
+//     asNavFor: ".slidersthree",
+//   });
+// });
+$(document).ready(function () {
+  $(".slidersthree").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    asNavFor: ".slidersone",
+  });
 });
