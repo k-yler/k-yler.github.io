d43ef10a79d2d0a7e2631013ee137c2a4da90f40
k-yler <68955748+k-yler@users.noreply.github.com>
2020-10-23 14:43:42 +0300

    Add files via upload

---
+++ login/lib.js
@@ -0,0 +1,56 @@
+let button = document.getElementById("button");
+
+let users = [
+    {
+        userName: "UserOne",
+        userLogin: "UserOneLog",
+        userPassword: "UserOnePas"
+    },
+    {
+        userName: "UserTwo",
+        userLogin: "UserTwo",
+        userPassword: "UserTwo"
+    },
+    {
+        userName: "UserThree",
+        userLogin: "UserThree",
+        userPassword: "UserThree"
+    },
+    {
+        userName: "Жопка",
+        userLogin: "alena",
+        userPassword: "1234"
+    },
+    {
+        userName: "MoRiNaD =)))))))",
+        userLogin: "marina",
+        userPassword: "1234"
+    },
+    {
+        userName: "admin",
+        userLogin: "admin",
+        userPassword: "admin"
+    }
+]
+
+function muFunc(){
+    let password = document.getElementById("inputPassword");
+    let login = document.getElementById("inputLogin");
+    let doneAut = 0;
+    for(let i = 0; i < users.length; i++){
+        if(login.value == users[i].userLogin && password.value == users[i].userPassword){
+            alert("Привет, " + users[i].userName);
+            password.value = "";
+            login.value = "";
+            doneAut = 1;
+        } 
+    }
+    if(doneAut == 0){
+        alert("Что-то пошло не так...");
+        password.value = "";
+        login.value = "";
+    }
+}
+
+button.addEventListener("click", muFunc);
+
