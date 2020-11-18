9611ab926951e1f56196b7c167d6d09078306409
k-yler <68955748+k-yler@users.noreply.github.com>
2020-11-18 16:03:45 +0200

    Add files via upload

--- weather/lib.js
+++ weather/lib.js
@@ -11,7 +11,7 @@ let nav = navigator.geolocation.getCurrentPosition(pos => {
     long = pos.coords.longitude
     console.log(long);
     let apiKey = '8c6382a4c537bb614eb1ac5d1a8bea31';
-    let proxy = `https://cors-anywhere.herokuapp.com/`
+    let proxy = `https://cors-anywhere.herokuapp.com/`;
     let api = `${proxy}api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&lang=en`;
     try{
         fetch(api).then(response =>{
@@ -33,6 +33,28 @@ function showWeather(data){
     weather.innerHTML = JSON.stringify(data.weather[0].description).replace(/"/g, "");
     let tmp = JSON.stringify(data.main.temp);
     tmp = Number(tmp).toFixed()-273;
-    degree.innerHTML = tmp
+    degree.innerHTML = " " + tmp;
     sity.innerHTML = JSON.stringify(data.name).replace(/"/g, "");
 }
+
+function showWeatherOfSity(){
+    let apiKey = '8c6382a4c537bb614eb1ac5d1a8bea31';
+    let proxy = `https://cors-anywhere.herokuapp.com/`
+    let cityName = document.querySelector("input").value;
+    let api = `${proxy}api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+    try{
+        fetch(api).then(response =>{
+            return response.json().then(data => {
+                showWeather(data);
+                console.log(data);
+            })
+        })
+    } catch (e){
+        console.log(e);
+    }
+}
+// showWeatherOfSity();
+
+document.querySelector("button").addEventListener("click", showWeatherOfSity);
+
+
