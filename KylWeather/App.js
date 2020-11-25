955c083380ff82115067591a67606f0128186781
k-yler <68955748+k-yler@users.noreply.github.com>
2020-11-25 20:01:00 +0200

    Add files via upload

--- KylWeather/App.js
+++ KylWeather/App.js
@@ -1,6 +1,7 @@
 const APIKEY = '8c6382a4c537bb614eb1ac5d1a8bea31';
+let load = document.querySelector(".loading");
 
-const otherDays =[]
+// const otherDays =[]
 
 if(navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(success, error);
@@ -22,6 +23,7 @@ const startApp = (lat, long) =>{
 }
 
 const getData = url => {
+    loadPlay();
     fetch(url).then((response) => response.json()).then(data => {
         // console.log(data);
         if(data.cod == 404){
@@ -47,7 +49,7 @@ const createWeather = data => {
         feelsTemp: data.list[0].main.feels_like.toFixed()-273,
         temp: data.list[0].main.temp.toFixed()-273,
         description: data.list[0].weather[0].description,
-        icon: data.list[0].weather[0].icon,
+        icon: getChangedIcon(data.list[0].weather[0].icon),
         wind: data.list[0].wind.speed + "m/s"
     }
 }
@@ -71,8 +73,40 @@ const renderMainCard = weather => {
     sunrise.innerHTML = weather.sunrise;
     sunset.innerHTML = weather.sunset;
     pressure.innerHTML = weather.pressure;
+    // console.log(weather.icon, " icon");
 
     var skycons = new Skycons({"color": "white"});
-    skycons.add("icon1", Skycons.SNOW);
+    skycons.add("icon1", weather.icon);
+    // console.log(tmp);
     skycons.play();
+    loadStop();
+}
+
+const getChangedIcon = icon => {
+    console.log(icon);
+    if(icon == "01d") return Skycons.Skycnos.CLEAR_DAY;
+    if(icon == "01n") return Skycons.Skycnos.CLEAR_NIGHT;
+    if(icon == "02d") return Skycons.PARTLY_CLOUD_DAY;
+    if(icon == "02n") return Skycons.PARTLY_CLOUD_NIGHT;
+    if(icon == "03d") return Skycons.CLOUDY;
+    if(icon == "03n") return Skycons.CLOUDY;
+    if(icon == "04d") return Skycons.CLOUDY;
+    if(icon == "04n") return Skycons.CLOUDY;
+    if(icon == "09d") return Skycons.SLEET;
+    if(icon == "09n") return Skycons.SLEET;
+    if(icon == "10d") return Skycons.RAIN;
+    if(icon == "10n") return Skycons.RAIN;
+    if(icon == "11d") return Skycnos.RAIN;
+    if(icon == "11n") return Skycnos.RAIN;
+    if(icon == "13n") return Skycnos.SNOW;
+    if(icon == "13d") return Skycnos.SNOW;
+    if(icon == "50n") return Skycnos.FOG;
+    if(icon == "50d") return Skycnos.FOG;
+}
+
+const loadPlay = () =>{
+    load.style.display = "block";
+}
+const loadStop = () => {
+    load.style.display = "none";
 }
