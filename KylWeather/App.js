8d9d5317f554f55b16b1ee35271db0c90b6dd0b9
k-yler <68955748+k-yler@users.noreply.github.com>
2020-11-26 17:58:10 +0200

    Add files via upload

--- KylWeather/App.js
+++ KylWeather/App.js
@@ -1,5 +1,12 @@
 const APIKEY = '8c6382a4c537bb614eb1ac5d1a8bea31';
 let load = document.querySelector(".loading");
+const button = document.querySelector(".get");
+const ICONS = {
+    '01d': Skycons.CLEAR_DAY, '01n': Skycons.CLEAR_NIGHT, '02d': Skycons.PARTLY_CLOUD_DAY, '02n': Skycons.PARTLY_CLOUD_NIGHT, '03d': Skycons.CLOUDY, '03n': Skycons.CLOUDY, '04d': Skycons.CLOUDY, '04n': Skycons.CLOUDY, '09d': Skycons.SLEET, '09n': Skycons.SLEET, '10d': Skycons.RAIN, '10n': Skycons.RAIN, "11d":Skycons.RAIN,"11n": Skycons.RAIN, "13n": Skycons.SNOW, "13d": Skycons.SNOW, "50n": Skycons.FOG, "50d": Skycons.FOG
+};
+const dayOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thusday', 'Friday', "Saturday"];
+const mouth = ['Jun', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+
 
 // const otherDays =[]
 
@@ -22,6 +29,21 @@ const startApp = (lat, long) =>{
     getData(url);
 }
 
+button.addEventListener("click", changeCountry);
+function changeCountry() {
+    const input = document.querySelector("input");
+    if(input.value){
+        console.log(111);
+        console.log(input.value);
+        const url = `https://api.openweathermap.org/data/2.5/forecast?q=${input.value.trim()}&appid=${APIKEY}`;
+        input.value = "";
+        getData(url);    
+    }
+}
+document.addEventListener("keydown", function (e){
+    if (e.keyCode === 13) changeCountry();
+})
+
 const getData = url => {
     loadPlay();
     fetch(url).then((response) => response.json()).then(data => {
@@ -41,15 +63,15 @@ const createWeather = data => {
     const sunriseD = new Date(data.city.sunrise * 1000);
     const sunsetD = new Date(data.city.sunset * 1000);
     return {
-        sunrise: `${sunriseD.getHours()<10 ? "0" + sunriseD.getHours() : sunriseD.getHours()}:${sunriseD.getMinutes()}`,
-        sunset: `${sunsetD.getHours()<10 ? "0" + sunsetD.getHours() : sunsetD.getHours()}:${sunsetD.getMinutes()}`,
+        sunrise: `${sunriseD.getHours()<10 ? "0" + sunriseD.getHours() : sunriseD.getHours()}:${sunriseD.getMinutes()<10 ? "0" + sunriseD.getMinutes() : sunriseD.getMinutes()}`,
+        sunset: `${sunsetD.getHours()<10 ? "0" + sunsetD.getHours() : sunsetD.getHours()}:${sunsetD.getMinutes()<10 ? "0" + sunsetD.getMinutes() : sunsetD.getMinutes()}`,
         name: data.city.name,
         country: data.city.country,
-        pressure: data.list[0].main.pressure,
+        pressure: (data.list[0].main.pressure / 1.3333).toFixed(),
         feelsTemp: data.list[0].main.feels_like.toFixed()-273,
         temp: data.list[0].main.temp.toFixed()-273,
         description: data.list[0].weather[0].description,
-        icon: getChangedIcon(data.list[0].weather[0].icon),
+        icon: ICONS[data.list[0].weather[0].icon],
         wind: data.list[0].wind.speed + "m/s"
     }
 }
@@ -65,7 +87,7 @@ const renderMainCard = weather => {
     const wind = document.querySelector(".wind");
     const sunset = document.querySelector(".sunset");
     // console.log(degree);
-    city.innerHTML = weather.name;
+    city.innerHTML = `${weather.country}, ${weather.name}`;
     degree.innerHTML = weather.temp;
     description.innerHTML = weather.description;
     feelsLike.innerHTML = weather.feelsTemp;
@@ -80,28 +102,6 @@ const renderMainCard = weather => {
     // console.log(tmp);
     skycons.play();
     loadStop();
-}
-
-const getChangedIcon = icon => {
-    console.log(icon);
-    if(icon == "01d") return Skycons.Skycnos.CLEAR_DAY;
-    if(icon == "01n") return Skycons.Skycnos.CLEAR_NIGHT;
-    if(icon == "02d") return Skycons.PARTLY_CLOUD_DAY;
-    if(icon == "02n") return Skycons.PARTLY_CLOUD_NIGHT;
-    if(icon == "03d") return Skycons.CLOUDY;
-    if(icon == "03n") return Skycons.CLOUDY;
-    if(icon == "04d") return Skycons.CLOUDY;
-    if(icon == "04n") return Skycons.CLOUDY;
-    if(icon == "09d") return Skycons.SLEET;
-    if(icon == "09n") return Skycons.SLEET;
-    if(icon == "10d") return Skycons.RAIN;
-    if(icon == "10n") return Skycons.RAIN;
-    if(icon == "11d") return Skycnos.RAIN;
-    if(icon == "11n") return Skycnos.RAIN;
-    if(icon == "13n") return Skycnos.SNOW;
-    if(icon == "13d") return Skycnos.SNOW;
-    if(icon == "50n") return Skycnos.FOG;
-    if(icon == "50d") return Skycnos.FOG;
 }
 
 const loadPlay = () =>{
